2c4968b3e857a6dc9321b0111da193046d3f0a96
viowla <55015538+viowla@users.noreply.github.com>
2020-10-12 09:09:59 +0600

    edit eurika client

--- src/main/java/kz/iitu/genresservice/GenresServiceApplication.java
+++ src/main/java/kz/iitu/genresservice/GenresServiceApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.genresservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GenresServiceApplication {
 
     public static void main(String[] args) {
