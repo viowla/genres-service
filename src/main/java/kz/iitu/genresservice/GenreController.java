b5630a637f9b71fe57a1d897a9db129c613ca042
viowla <55015538+viowla@users.noreply.github.com>
2020-10-12 09:49:48 +0600

    add new request

--- src/main/java/kz/iitu/genresservice/GenreController.java
+++ src/main/java/kz/iitu/genresservice/GenreController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 @RequestMapping("/genre")
 public class GenreController {
@@ -12,5 +14,17 @@ public class GenreController {
     @GetMapping("/{movieId}")
     public Genre getGenreByMovieId(@PathVariable("movieId") String movieId){
         return new Genre("1", movieId, "default");
+    }
+
+    @GetMapping("")
+    public List<Genre> getAllGenres(){
+
+        List<Genre> genreList = new ArrayList<>();
+        genreList.add(new Genre("1","1","Action"));
+        genreList.add(new Genre("2","2","Horror"));
+        genreList.add(new Genre("3","3","Comedy"));
+        genreList.add(new Genre("4","4","Thriller"));
+        genreList.add(new Genre("5","5","Sci-fi"));
+        return genreList;
     }
 }
