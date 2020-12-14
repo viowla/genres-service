0701f5197401b4db4c8daca195d1e762787d708a
viowla <55015538+viowla@users.noreply.github.com>
2020-12-15 00:15:09 +0600

    edit genre service/controller

--- src/main/java/kz/iitu/genresservice/GenreController.java
+++ src/main/java/kz/iitu/genresservice/GenreController.java
@@ -2,29 +2,41 @@ package kz.iitu.genresservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin("*")
 @RequestMapping("/genre")
 public class GenreController {
 
 
-    @GetMapping("")
+    @Autowired
+    private GenreRepository genreRepository;
+
+
+    private GenreService genreService;
+
+    @GetMapping("/all")
     @HystrixCommand(fallbackMethod = "getPayment",
             threadPoolKey = "getPaymentPool",
             threadPoolProperties = {
                     @HystrixProperty(name="coreSize", value="20"),
                     @HystrixProperty(name="maxQueueSize", value="10"),
             })
-    public List<Genre> getAllGenres(){
-
-        List<Genre> genreList = new ArrayList<>();
-
-        return genreList;
+    public ResponseEntity<List<Genre>> getAllPayments() {
+        return new ResponseEntity<>(genreRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Genre> getPayment(@PathVariable String id) {
+        return new ResponseEntity<>(genreService.getGenre(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/kz/iitu/genresservice/GenreService.java
+++ src/main/java/kz/iitu/genresservice/GenreService.java
@@ -1,6 +1,38 @@
 package kz.iitu.genresservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
 public class GenreService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @HystrixCommand(fallbackMethod = "getPayment",
+            threadPoolKey = "getPaymentPool",
+            threadPoolProperties = {
+                    @HystrixProperty(name="coreSize", value="20"),
+                    @HystrixProperty(name="maxQueueSize", value="10"),
+            })
+    public Genre getGenre(String id) {
+
+        String apiCredentials = "rest-client:p@ssword";
+        String base64Credentials = new String(Base64.encodeBase64(apiCredentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange("http://genre-service/id/"+id, HttpMethod.GET, entity,Genre.class).getBody();
+
+    }
 
 
 }
